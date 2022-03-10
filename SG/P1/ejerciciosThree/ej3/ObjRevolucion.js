927ac3a756368d76af565b316b7e9cf91b86d072
PinGunter <salvaromero08@gmail.com>
2022-03-10 16:44:23 +0100

    terminado ejercicio 3

---
+++ SG/P1/ejerciciosThree/ej3/ObjRevolucion.js
@@ -0,0 +1,91 @@
+import * as THREE from '../libs/three.module.js'
+
+class ObjRevolucion extends THREE.Object3D {
+    constructor(gui, titleGui){
+        super();
+
+        this.puntos = [];
+
+        this.angle = Math.PI;
+        this.resolution = 5;
+       
+        // Puntos que definen al peon
+        this.puntos.push(new THREE.Vector3(0.0,-1.4,0.0));
+        this.puntos.push(new THREE.Vector3(1.0, -1.4, 0.0));
+        this.puntos.push(new THREE.Vector3(1.0, -1.1, 0.0));
+        this.puntos.push(new THREE.Vector3(0.5, -0.7, 0.0));
+        this.puntos.push(new THREE.Vector3(0.4, -0.4, 0.0));
+        this.puntos.push(new THREE.Vector3(0.4, 0.5, 0.0));
+        this.puntos.push(new THREE.Vector3(0.5, 0.6, 0.0));
+        this.puntos.push(new THREE.Vector3(0.3, 0.6, 0.0));
+        this.puntos.push(new THREE.Vector3(0.5, 0.8, 0.0));
+        this.puntos.push(new THREE.Vector3(0.55, 1.0, 0.0));
+        this.puntos.push(new THREE.Vector3(0.5, 1.2, 0.0));
+        this.puntos.push(new THREE.Vector3(0.3, 1.4, 0.0));
+        this.puntos.push(new THREE.Vector3(0.0,1.4,0.0));
+        
+        
+        if (gui != null && titleGui != null){
+            this.createGUI(gui, titleGui);
+        }
+
+
+        // Creacion de la malla
+
+        var geo = new THREE.LatheGeometry(this.puntos, this.resolution, 0, this.angle);
+        this.material = new THREE.MeshNormalMaterial({flatShading: true})
+        this.material.needsUpdate = true;
+        this.obj = new THREE.Mesh(geo, this.material);
+
+        // Creacion de una linea visible
+        var lineGeometry = new THREE.BufferGeometry();
+        lineGeometry.setFromPoints(this.puntos);
+        this.line = new THREE.Line(lineGeometry, this.material);
+
+        this.obj.position.set(2.5,1.4,0);
+        this.line.position.set(-2.5,1.4,0);
+        this.add(this.obj);
+        this.add(this.line);
+    }
+
+    createGUI(gui, titleGui){
+        this.guiControls = {
+            resolution: this.resolution,
+            angle: this.angle,
+        };
+
+        var folder = gui.addFolder(titleGui);
+
+        folder.add(this.guiControls, "resolution", 1, 15, 1).name("Resolución").onChange( (value) => this.updateRes(value) );
+        folder.add(this.guiControls, "angle", 1, 2*Math.PI, 0.5).name("Ángulo").onChange( (value) => this.updateAngle(value) );
+
+    }
+
+    reconstruir(){
+        this.obj.geometry.dispose();
+        this.remove(this.obj);
+        var geoNueva = new THREE.LatheGeometry(this.puntos, this.resolution, 0, this.angle);        
+        this.obj = new THREE.Mesh(geoNueva, this.material);
+        this.obj.position.set(2.5,1.4,0);
+        this.add(this.obj);
+
+    }
+
+    updateAngle(angle){
+        this.angle = angle;
+        this.reconstruir();
+    }
+
+    updateRes(res){
+        this.resolution = res;
+        this.reconstruir();
+    }
+
+    update(){
+        this.obj.position.set(2.5,1.4,0);
+        this.line.position.set(-2.5,1.4,0);
+    }
+    
+}
+
+export { ObjRevolucion };
