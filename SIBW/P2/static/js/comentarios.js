af7122fdba8f084a9f215aa37b3a9028a41e99c2
Salva Romero <salvaromero08@gmail.com>
2022-03-22 14:46:02 +0100

    jijija

--- SIBW/P2/static/js/comentarios.js
+++ SIBW/P2/static/js/comentarios.js
@@ -1,9 +1,25 @@
 function mostrarComentarios(){
     var comentarios = document.getElementById("seccion-comentarios");
     var visible = comentarios.getAttribute("class") === "comentarios-no-visibles";
+    var boton = document.getElementById("toggle-comments");
     if (visible){
         comentarios.setAttribute("class", "comentarios-visibles");
+        boton.innerHTML = "<i class=\"fa fa-comment\"> Ocultar comentarios</i>";
     } else{
         comentarios.setAttribute("class", "comentarios-no-visibles");
+        boton.innerHTML = "<i class=\"fa fa-comment\"> Mostrar comentarios</i>";
     }
 }
+
+function censuraComentarios(){
+    var comenario = document.getElementById("comment-form").value;
+    if (comentario.includes("tonto")){
+        comentario.replace("tonto", "*****");
+    }
+}
+
+var emailInput = document.getElementById("email-form");
+emailInput.addEventListener("InputEvent",censuraComentarios());
+var botonEnviar = document.getElementById("send-form");
+alert("ola");
+// botonEnviar.addEventListener();
