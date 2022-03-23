aa36ed14114f573cfebbbe4fa3f0e7c5eeaa1f3e
Salva Romero <salvaromero08@gmail.com>
2022-03-23 18:41:08 +0100

    como antes pero mejor xd

--- SIBW/P2/static/js/comentarios.js
+++ SIBW/P2/static/js/comentarios.js
@@ -4,22 +4,12 @@ function mostrarComentarios(){
     var boton = document.getElementById("toggle-comments");
     if (visible){
         comentarios.setAttribute("class", "comentarios-visibles");
-        boton.innerHTML = "<i class=\"fa fa-comment\"> Ocultar comentarios</i>";
+        boton.innerHTML = "<a><i class=\"fa fa-comment \"></i> Ocultar comentarios</a>";
     } else{
         comentarios.setAttribute("class", "comentarios-no-visibles");
-        boton.innerHTML = "<i class=\"fa fa-comment\"> Mostrar comentarios</i>";
+        boton.innerHTML = "<a><i class=\"fa fa-comment \"></i> Mostrar comentarios</a>";
     }
 }
+botonComentarios = document.getElementById("toggle-comments");
+botonComentarios.addEventListener("click", mostrarComentarios);
 
-function censuraComentarios(){
-    var comenario = document.getElementById("comment-form").value;
-    if (comentario.includes("tonto")){
-        comentario.replace("tonto", "*****");
-    }
-}
-
-var emailInput = document.getElementById("email-form");
-emailInput.addEventListener("InputEvent",censuraComentarios());
-var botonEnviar = document.getElementById("send-form");
-alert("ola");
-// botonEnviar.addEventListener();
