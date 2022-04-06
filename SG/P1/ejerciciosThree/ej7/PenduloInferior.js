546a56a1ea45aef51a5a61f424fbe760b210a29d
Salva Romero <salvaromero08@gmail.com>
2022-04-06 16:51:31 +0200

    :sunglasses: para nada es un commit para meter el emoji

---
+++ SG/P1/ejerciciosThree/ej7/PenduloInferior.js
@@ -0,0 +1,10 @@
+import * as THREE from '../libs/three.module.js'
+
+class PenduloInferior extends THREE.Object3D{
+    constructor(title, titleGUI){
+        super();
+        /*
+            Implementarlo 😎
+        */
+    }
+}
